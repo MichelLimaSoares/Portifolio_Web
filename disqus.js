1c70297c02926b30f9ecf4191ea25a0471d9a066
michel.soares <michellimasoares2017@gmail.com>
2023-10-23 16:53:01 -0300

    update - Test disqus

---
+++ disqus.js
@@ -0,0 +1,13 @@
+            
+var disqus_config = function () {
+this.page.url = 'https://portifolio-mls-tec.netlify.app/';  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = 'https://portifolio-mls-tec.netlify.app/'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() {
+var d = document, s = d.createElement('script');
+s.src = 'https://https-portifolio-mls-tec-netlify-app.disqus.com/embed.js';  // substitua 'your-disqus-shortname' pelo seu shortname do Disqus
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+            
