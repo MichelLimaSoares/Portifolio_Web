ab84af394eb17f320fe5e1ec6ea0165036c3bb1f
michel.soares <michellimasoares2017@gmail.com>
2023-10-24 14:04:00 -0300

    update - Tasks wipro

--- experience.js
+++ experience.js
@@ -1,4 +1,4 @@
-function toggleVisibility() {
-    var element = document.getElementById("experience_tasks");
+function toggleVisibility(id) {
+    var element = document.getElementById(id);
     element.classList.toggle("grid-task-experience-hidden");
 }
